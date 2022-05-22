beb0fe2559865bf465d21060863f93d475890a2c
Igor Kowalewski <kowalewski.igor.ug@gmail.com>
2022-05-22 12:42:47 +0200

    30. New create view to add recipes 31. Autocomplete - ingredients search by name

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -1,11 +1,14 @@
 ﻿using FoodEaterRecipes.Data;
 using FoodEaterRecipes.Models;
 using FoodEaterRecipes.Services;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -17,12 +20,16 @@ namespace FoodEaterRecipes.Controllers
     {
         private readonly IMailService _mailService;
         private readonly IFoodEaterRepository _repository;
+        private readonly IWebHostEnvironment _environment;
 
-        public AppController(IMailService mailService, IFoodEaterRepository repository)
+
+        public AppController(IMailService mailService, IFoodEaterRepository repository, IWebHostEnvironment environment)
         {
             _mailService = mailService;
             _repository = repository;
+            _environment = environment;
         }
+
 
         public IActionResult Index()
         {
@@ -35,8 +42,11 @@ namespace FoodEaterRecipes.Controllers
         {
             ViewData["Recipes"] = new SelectList(_repository.GetAllRecipes(), "Id", "Name");
             ViewBag.SearchResultRecipes = _repository.GetAllRecipes();
+            ViewBag.AbsolutePath = _environment.WebRootPath + $"\\src\\";
+
             return View();
         }
+
 
         [HttpPost("Recipes")]
         public JsonResult Recipes(string Prefix)
@@ -45,11 +55,63 @@ namespace FoodEaterRecipes.Controllers
         }
 
 
-        [HttpGet("About")]
-        public async Task<ActionResult> AboutUs()
+        [HttpGet("Create")]
+        public async Task<ActionResult> Create()
         {
+
+            // WYŁĄCZAM TO LISTOWANIE PRZY URUCHAMIANIU FUNKCJONALNOSCI
+            //string Baseurl = "https://trackapi.nutritionix.com/";
+            //IngredientsSearchAPI ResultList = new();
+
+            //using (HttpClient client = new())
+            //{
+            //    //Passing service base url
+            //    client.BaseAddress = new Uri(Baseurl);
+            //    client.DefaultRequestHeaders.Clear();
+
+            //    //Define request data format
+            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //    client.DefaultRequestHeaders.Add("x-app-id", "551bc1ca");
+            //    client.DefaultRequestHeaders.Add("x-app-key", "7e6ed04c9d8c38bc4b7052fbdecf188c");
+
+            //    //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+            //    HttpResponseMessage Res = await client.GetAsync("v2/search/instant?query=white bean");
+
+            //    //Checking the response is successful or not which is sent using HttpClient
+            //    if (Res.IsSuccessStatusCode)
+            //    {
+            //        //Storing the response details recieved from web api
+            //        var ResultResponse = Res.Content.ReadAsStringAsync().Result;
+
+            //        //Deserializing the response recieved from web api and storing into the Employee list
+            //        ResultList = JsonConvert.DeserializeObject<IngredientsSearchAPI>(ResultResponse);
+            //        //returning the list to view
+
+            //        return View(ResultList);
+            //    }
+
+            //}
+            
+            return View();
+        }
+
+
+        [HttpPost("Create")]
+        public JsonResult Create(string Prefix)
+        {
+
+            var APIrequestTask = GetIngridientsFromAPI(Prefix);
+            APIrequestTask.Wait();
+            List<string> ingredientList = APIrequestTask.Result;
+
+            return Json(ingredientList);
+        }
+
+        private async Task<List<string>> GetIngridientsFromAPI(string Prefix)
+        {
+            List<string> response = new();
+            IngredientsSearchAPI ResultList = new IngredientsSearchAPI();
             string Baseurl = "https://trackapi.nutritionix.com/";
-            IngredientsSearchAPI ResultList = new();
 
             using (HttpClient client = new())
             {
@@ -63,7 +125,7 @@ namespace FoodEaterRecipes.Controllers
                 client.DefaultRequestHeaders.Add("x-app-key", "7e6ed04c9d8c38bc4b7052fbdecf188c");
 
                 //Sending request to find web api REST service resource GetAllEmployees using HttpClient
-                HttpResponseMessage Res = await client.GetAsync("v2/search/instant?query=white bean");
+                HttpResponseMessage Res = await client.GetAsync($"v2/search/instant?query={Prefix}");
 
                 //Checking the response is successful or not which is sent using HttpClient
                 if (Res.IsSuccessStatusCode)
@@ -73,13 +135,22 @@ namespace FoodEaterRecipes.Controllers
 
                     //Deserializing the response recieved from web api and storing into the Employee list
                     ResultList = JsonConvert.DeserializeObject<IngredientsSearchAPI>(ResultResponse);
-                    //returning the list to view
 
-                    return View(ResultList);
+                    foreach(var c in ResultList.Common)
+                    {
+                        response.Add(c.Food_name);
+                    }
                 }
-
-                return View();
             }
+
+            return response;
+        }
+
+
+        [HttpGet("About")]
+        public IActionResult AboutUs()
+        {
+            return View();
         }
 
 
@@ -97,8 +168,8 @@ namespace FoodEaterRecipes.Controllers
             {
                 // Valid POST
                 _mailService.SendMessage("igor.kowalewski.ug@gmail.com"
-                    ,contactMessage.Subject
-                    ,$"From: {contactMessage.Name} " +
+                    , contactMessage.Subject
+                    , $"From: {contactMessage.Name} " +
                      $"Email: {contactMessage.Email} " +
                      $"Message: {contactMessage.Message}");
 
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -23,12 +23,12 @@ $(document).ready(function () {
         console.log("User clicked on " + $(this).text());
     });
 
-    var SearchInput = $("#SearchInputRecipes");
-    SearchInput.on("input", function () {
+    var SearchInputRecipes = $("#SearchInputRecipes");
+    SearchInputRecipes.on("input", function () {
 
         console.log("User typing...");
 
-        SearchInput.autocomplete({
+        SearchInputRecipes.autocomplete({
             source: function (request, response) {
                 $.ajax({
                     url: "/Recipes",
@@ -39,6 +39,36 @@ $(document).ready(function () {
                         response($.map(data, function (value) {
                             console.log(value.name);
                             return { label: value.name, value: value.name };
+                        }))
+                    }
+                })
+            },
+            messages: {
+                noResults: 'No results found.',
+                results: function () {
+                    return;
+                }
+            }
+        });
+    });
+
+
+    var SearchInputIngredient = $("#SearchInputIngredient");
+    SearchInputIngredient.on("input", function () {
+
+        console.log("User typing...");
+
+        SearchInputIngredient.autocomplete({
+            source: function (request, response) {
+                $.ajax({
+                    url: "/Create",
+                    type: "POST",
+                    dataType: "json",
+                    data: { Prefix: request.term },
+                    success: function (data) {
+                        response($.map(data, function (value) {
+                            console.log(value);
+                            return { label: value, value: value };
                         }))
                     }
                 })
